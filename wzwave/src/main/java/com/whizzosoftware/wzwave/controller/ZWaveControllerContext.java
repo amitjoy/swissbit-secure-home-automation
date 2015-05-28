5956f23ed309322c8b2451e9552f665d09192199
Amit Kumar Mondal <admin@amitinside.com>
2015-05-28 21:22:38 +0200

    Added wzwave-0.0.4-SNAPSHOT.jar to POM and Deployment Package Removed the WZWave source

--- wzwave/src/main/java/com/whizzosoftware/wzwave/controller/ZWaveControllerContext.java
+++
@@ -1,7 +0,0 @@
-package com.whizzosoftware.wzwave.controller;
-
-import com.whizzosoftware.wzwave.frame.DataFrame;
-
-public interface ZWaveControllerContext {
-    public void sendDataFrame(DataFrame frame);
-}
