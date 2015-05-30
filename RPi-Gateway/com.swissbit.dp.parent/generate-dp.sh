03936c32e287df0f54f99a74337fabbce4f46cb2
Amit Kumar Mondal <admin@amitinside.com>
2015-05-30 12:07:39 +0200

    Modified Directory Locations in Deployment Package and Ant Scripts

--- RPi-Gateway/com.swissbit.dp.parent/generate-dp.sh
+++ RPi-Gateway/com.swissbit.dp.parent/generate-dp.sh
@@ -27,7 +27,7 @@ b="_build.xml"
 c="/"
 
 # The root directory of all the aforementioned projects.
-home_dir="/Users/AMIT/Praktikum/swissbit/"
+home_dir="/Users/AMIT/swissbit/RPi-Gateway/"
 
 # The DP file extension
 ext=".dp"
