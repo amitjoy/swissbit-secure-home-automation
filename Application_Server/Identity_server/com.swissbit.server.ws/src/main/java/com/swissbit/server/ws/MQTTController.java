132e4f8be5ea18862462e020e9932ec8b4c29adc
Amit Kumar Mondal <admin@amitinside.com>
2015-06-08 13:38:05 +0200

    Refactored Code

--- Application_Server/Identity_server/com.swissbit.server.ws/src/main/java/com/swissbit/server/ws/MQTTController.java
+++ Application_Server/Identity_server/com.swissbit.server.ws/src/main/java/com/swissbit/server/ws/MQTTController.java
@@ -9,6 +9,8 @@ public class MQTTController {
 
 	public MQTTController(final IMQTTService mqttService) {
 
+		// Used to ask the provided Raspberry Pi whether the Mobile client who
+		// is trying to add it, is authorized (mainly used by mobile client)
 		get("/addRPi/:rPiMacAddress/:mobileClientMacAddress/", (req, res) -> {
 			final String rPiMacAddress = req.params(":rPiMacAddress");
 			final String mobileClientMacAddress = req.params(":mobileClientMacAddress");
