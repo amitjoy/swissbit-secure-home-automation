9c6550126db36bcfb6098c263d2c50187e7a65a5
Gaurav Srivastava <gaurav.srivastava7@gmail.com>
2015-06-10 21:25:39 +0200

    Integrated the changes for OpenShift deployment. Accessing all environment variables in Constants.java

--- Application_Server/com.swissbit.identity.server/src/com/swissbit/server/ws/Constants.java
+++ Application_Server/com.swissbit.identity.server/src/com/swissbit/server/ws/Constants.java
@@ -2,10 +2,12 @@ package com.swissbit.server.ws;
 
 public interface Constants {
 
-	public static final String DB_URL = "jdbc:mysql://localhost/spark";
-	public static final String IP_ADDRESS = "";
-	public static final String MYSQL_PASSWORD = "root";
-	public static final String MYSQL_USER = "root";
-	public static final int PORT = 4567;
+	public static final String DB_URL = "jdbc:mysql://" + System.getenv().get("OPENSHIFT_MYSQL_DB_HOST") + ":" 
+										+ System.getenv().get("OPENSHIFT_MYSQL_DB_PORT") + "/andropraktikumtum";
+	
+	public static final String IP_ADDRESS = System.getenv().get("OPENSHIFT_DIY_IP");
+	public static final String MYSQL_USER = System.getenv().get("OPENSHIFT_MYSQL_DB_USERNAME");
+	public static final String MYSQL_PASSWORD = System.getenv().get("OPENSHIFT_MYSQL_DB_PASSWORD");
+	public static final String PORT = System.getenv().get("OPENSHIFT_DIY_PORT");	
 
 }
