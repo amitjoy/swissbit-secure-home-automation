38a5c19d99c3eaa3a7805e3fc87713cf08f377c1
Gaurav Srivastava <gaurav.srivastava7@gmail.com>
2015-06-16 19:46:25 +0200

    Updated the Encryption and Decryption API and API Tester API to incorporate Java Card applet customisations

--- Secure_Element/SSDAPI/app/src/main/java/tum/com/ssdapi/SSDAPIMain.java
+++ Secure_Element/SSDAPI/app/src/main/java/tum/com/ssdapi/SSDAPIMain.java
@@ -2,8 +2,7 @@ package tum.com.ssdapi;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.content.Intent;
 
 import com.secureflashcard.sfclibrary.SfcTerminal;
@@ -15,17 +14,27 @@ public class SSDAPIMain extends Activity {
     Intent iData;
     byte[] atr = null;
     byte[] response = null;
-
+    String plainMessage = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ssdapimain);
 
+        Log.d("API: ", "onCreate function in SSD API");
         iData = new Intent();
+
+        /*
+         * Based on the Function Id of the Request Identify which functionality is called in the API
+         * 1006  :  Encrypt
+         * 1007  :  Decrypt
+         * 1008  :  Get Card ID
+         * 1009  :  Check Card is Present.
+         */
 
         Bundle bundle = getIntent().getExtras();
         int function = bundle.getInt("Function");
+
 
         switch(function){
 
@@ -34,6 +43,8 @@ public class SSDAPIMain extends Activity {
              * The Message to be encrypted will be passed in Bundle as a parameter with name "MSG"
              */
             case 1006:
+
+                Log.d("Function Called: ", "Encrypt");
                 encryptMsg(bundle.getString("MSG"));
                 break;
 
@@ -42,6 +53,17 @@ public class SSDAPIMain extends Activity {
              * The Message to be encrypted will be passed in Bundle as a parameter with name "MSG"
              */
             case 1007:
+
+                Log.d("Function Called: ", "Decrypt");
+                decryptMsg(bundle.getByteArray("MSG"));
+                break;
+
+            /*
+             * Function code 1008 is used to Get the Unique ID of the Card present in the system.
+             */
+            case 1008:
+
+                Log.d("Function Called: ", "Get Card ID");
                 decryptMsg(bundle.getByteArray("MSG"));
                 break;
 
@@ -49,9 +71,14 @@ public class SSDAPIMain extends Activity {
              * Function code 1009, can be used to check whether the card is present or not.
              */
             case 1009:
+
+                Log.d("Function Called: ", "Card Present?");
                 isCardPresent();
                 break;
 
+            /*
+             * This is default call if any invalid function code is presented.
+             */
             default:
                 defaultCall();
                 break;
@@ -59,37 +86,16 @@ public class SSDAPIMain extends Activity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ssdapimain1, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void defaultCall(){
-
-        iData.putExtra("Response", "Card Present");
+        Log.d("Inside Function:", "Default Function call, Invalid Function code passed");
+        iData.putExtra("Response", "Invalid Function call.");
         setResult(android.app.Activity.RESULT_OK, iData);
         finish();
     }
 
     public void isCardPresent() {
+
+        Log.d("Inside Function:", "isCardPresent, Sets the response after the call.");
 
         connectCard();
 
@@ -114,6 +120,8 @@ public class SSDAPIMain extends Activity {
 
     public void encryptMsg(String msg) {
 
+        Log.d("Inside Function:", "encryptMsg, Sets the response with ByteArray (Encrypted String).");
+
         connectCard();
 
         try {
@@ -121,13 +129,64 @@ public class SSDAPIMain extends Activity {
             if (avail) {
                 atr = sfcTerminal.getAtr();
 
-                // library cares about keep alive, if app has android.permission.WAKE_LOCK permission!
-                sfcTerminal.connect();
-                byte[] cmd = new byte[] { (byte) 0x80, (byte) 0xCA, (byte) 0x9F, (byte)0x7F, (byte)0x00 };
-                response = sfcTerminal.transmit(cmd);
+                //Converting message to Byte Array.
+                byte[] msgBytes = msg.getBytes("UTF-8");
 
-                String responseMsg = bytesToString(response);
-                iData.putExtra("Response", responseMsg);
+                //Calculate the pad length because the message should be a multiple of 16
+                int padLength = (16 - ((msg.length()+1)%16))%16;
+
+                byte[] plainText = new byte[msg.length() + 1 +  padLength];
+
+                /*
+                 * String modifications:
+                 * Copy the byte array of original message to cipherText from byte 1,
+                 */
+                System.arraycopy (msgBytes, 0, plainText, 1, msgBytes.length);
+
+                /*
+                 * Byte 0 in Cipher Text contains the length of original message.
+                 * Payload length should be part of Crypto Gram Data (Protocol Design)
+                 */
+
+                plainText[0]= (byte)msgBytes.length;
+
+
+                // library cares about keep alive, if app has android.permission.WAKE_LOCK permission!
+
+                sfcTerminal.connect();
+
+                /*
+                 * This function is used to select the applet for further communication.
+                 * This step is mandatory cause without it the user will not be able to
+                 * talk to the applet.
+                 */
+                selectApplet();
+
+                byte[] cmd = new byte[] { (byte) 0x90, (byte) 0x21, (byte) 0x00, (byte)0x00, (byte)0x40 };
+
+                cmd[4] = (byte) plainText.length;
+
+                byte[] encryptCommand = new byte[cmd.length + plainText.length];
+
+                System.arraycopy (cmd, 0, encryptCommand, 0, cmd.length);
+                System.arraycopy (plainText, 0, encryptCommand, cmd.length, plainText.length);
+
+                Log.d("Encryption Command:", bytesToString(encryptCommand));
+
+                response = sfcTerminal.transmit(encryptCommand);
+
+                Log.d("Encrypted Message:", bytesToString(response));
+
+                /*
+                 * Should make a check that last 2 bytes are 90 00 which means everything went well.
+                 */
+                byte[] responseBytes = new byte[response.length - 2];
+                System.arraycopy (response, 0, responseBytes, 0, responseBytes.length);
+
+                iData.putExtra("Response", responseBytes);
+
+                Log.d("API returns encrypted:", bytesToString(responseBytes));
+
 
             } else {
                 iData.putExtra("Response", "Card Not Present");
@@ -146,25 +205,58 @@ public class SSDAPIMain extends Activity {
 
     public void decryptMsg(byte[] msg) {
 
+        Log.d("Inside Function:", "decryptMsg, Sets the response with 'Plain Text' .");
+
         connectCard();
 
         try {
+
             boolean avail = sfcTerminal.isCardPresent();
             if (avail) {
+
                 atr = sfcTerminal.getAtr();
+
+                Log.d("Byte Array Received:", bytesToString(msg));
+
+                byte[] msgBytes = msg;
+
 
                 // library cares about keep alive, if app has android.permission.WAKE_LOCK permission!
                 sfcTerminal.connect();
-                byte[] cmd = new byte[] { (byte) 0xFF, (byte) 0x70, (byte) 0x00, (byte)0x00, (byte)0x00 };
-                response = sfcTerminal.transmit(cmd);
 
-                String responseMsg = bytesToString(response);
-                iData.putExtra("Response", responseMsg);
+                /*
+                 * This function is used to select the applet for further communication.
+                 * This step is mandatory cause without it the user will not be able to
+                 * talk to the applet.
+                 */
+
+                selectApplet();
+
+
+                byte[] cmd = new byte[] { (byte) 0x90, (byte) 0x22, (byte) 0x00, (byte)0x00, (byte)0x40 };
+
+                cmd[4] = (byte)msgBytes.length;
+
+                byte[] decryptCommand = new byte[cmd.length + msgBytes.length];
+                System.arraycopy (cmd, 0, decryptCommand, 0, cmd.length);
+                System.arraycopy (msgBytes, 0, decryptCommand, cmd.length, msgBytes.length);
+
+                Log.d("Decryption Command:", bytesToString(decryptCommand));
+
+                response = sfcTerminal.transmit(decryptCommand);
+
+                this.plainMessage = getMessageFromResponse(response);
+
+                Log.d("Decrypted Message:", this.plainMessage);
+
+                iData.putExtra("Response", plainMessage);
+
             } else {
                 iData.putExtra("Response", "Card Not Present");
             }
 
         } catch (Exception e) {
+            iData.putExtra("Response", e.getMessage());
             e.printStackTrace();
         } finally {
 
@@ -194,6 +286,19 @@ public class SSDAPIMain extends Activity {
         }
     }
 
+    public void selectApplet() {
+        try {
+            byte[] cmd =  new byte[]{ (byte) 0x00, (byte) 0xA4, (byte) 0x04, (byte) 0x00, (byte) 0x0B,
+                                      (byte) 0xD2, (byte) 0x76, (byte) 0x00, (byte) 0x01, (byte) 0x62,
+                                      (byte) 0x44, (byte) 0x65, (byte) 0x6D, (byte) 0x6F, (byte) 0x01,
+                                      (byte) 0x01
+                                    };
+            sfcTerminal.transmit(cmd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private static String bytesToString(byte[] bytes) {
         StringBuffer sb = new StringBuffer();
         for (byte b : bytes) {
@@ -205,5 +310,26 @@ public class SSDAPIMain extends Activity {
         return sb.toString();
     }
 
+    /*
+     * This function is used to get the message from the decrypted string
+     * received from the card.
+     */
+    private String getMessageFromResponse(byte[] bytes) {
+        StringBuffer sb = new StringBuffer();
+        int lengthOfMessage = bytes[0];
+        Log.d("Length of Message: ", lengthOfMessage + "");
 
+        byte[] message = new byte[bytes.length - 1];
+
+        System.arraycopy(bytes, 1, message, 0, bytes.length - 1);
+
+        int counter = 1;
+        while (lengthOfMessage != 0){
+           sb.append((char) bytes[counter]);
+           counter ++;
+           lengthOfMessage--;
+        }
+
+        return sb.toString();
+    }
 }
--- Secure_Element/SSDAPITester/app/src/main/java/tum/com/myapplication/MainActivity.java
+++ Secure_Element/SSDAPITester/app/src/main/java/tum/com/myapplication/MainActivity.java
@@ -2,14 +2,16 @@ package tum.com.myapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.content.Intent;
 import android.app.Activity;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static byte[] message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,41 +25,52 @@ public class MainActivity extends ActionBarActivity {
          * For encrypt this would be a String Message
          * For decrypt this would be Byte Array
          */
-        Intent intent = new Intent("tum.com.ssdapi.MAIN_ACTIVITY");
-        Bundle bundle = new Bundle();
-        bundle.putInt("Function", 1006);
-        bundle.putString("MSG", "ABC");
-        intent.putExtras(bundle);
+        Button encryptButton = (Button) findViewById(R.id.encrypt);
+        encryptButton.setOnClickListener
+                (new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View v) {
+                          EditText plainMsg = (EditText) findViewById(R.id.encryptText);
+                          Intent intent = new Intent("tum.com.ssdapi.MAIN_ACTIVITY");
+                          Bundle bundle = new Bundle();
+                          bundle.putInt("Function", 1006);
+                          bundle.putString("MSG", plainMsg.getText().toString());
+                          intent.putExtras(bundle);
+                          /*
+                           * 6403 is REQUEST_CODE. This is just a number to uniquely identify a request.
+                           * The Function id is used to define the task, like encrypt or decrypt
+                           */
+                           startActivityForResult(intent, 6403);
+                        }
+                                         }
+        );
 
-        /*
-         * 6403 is REQUEST_CODE. This is just a number to uniquely identify a request.
-         * The Function id is used to define the task, like encrypt or decrypt
+         /*
+         * This part of the code is added to call the SSDAPI apk which will
+         * do the necessary task based on the Function code passed
+         * MSG parameter is used to pass a message. In case of status this parameter is not required
+         * For encrypt this would be a String Message
+         * For decrypt this would be Byte Array
          */
-        startActivityForResult(intent, 6403);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Button decryptButton = (Button) findViewById(R.id.decrypt);
+        decryptButton.setOnClickListener
+                (new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                         EditText encryptMsg = (EditText) findViewById(R.id.decryptText);
+                         Intent intent = new Intent("tum.com.ssdapi.MAIN_ACTIVITY");
+                         Bundle bundle = new Bundle();
+                         bundle.putInt("Function", 1007);
+                         bundle.putByteArray("MSG", message);
+                         intent.putExtras(bundle);
+                       /*
+                        * 6404 is REQUEST_CODE. This is just a number to uniquely identify a request.
+                        * The Function id is used to define the task, like encrypt or decrypt
+                        */
+                         startActivityForResult(intent, 6404);
+                     }
+                 }
+        );
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent pData)
@@ -66,13 +79,24 @@ public class MainActivity extends ActionBarActivity {
         {
             if (resultCode == Activity.RESULT_OK )
             {
-                final String zData = pData.getExtras().get("Response").toString();
+                final byte[] zData = pData.getExtras().getByteArray("Response");
 
-                Toast msg1 = Toast.makeText(this, zData, Toast.LENGTH_LONG );
-                msg1.show();
+                EditText resp = (EditText) findViewById(R.id.decryptText);
+                resp.setText("" + zData);
 
+                message = zData;
             }
         }
 
+        if ( requestCode == 6404 )
+        {
+            if (resultCode == Activity.RESULT_OK )
+            {
+                final String zData = pData.getExtras().get("Response").toString();
+
+                EditText resp = (EditText) findViewById(R.id.decryptText);
+                resp.setText(zData);
+            }
+        }
     }
 }
