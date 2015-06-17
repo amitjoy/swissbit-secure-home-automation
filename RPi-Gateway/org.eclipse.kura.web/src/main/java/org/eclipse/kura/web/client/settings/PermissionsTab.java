43f30dea8ebf5ce1d54898b6b089cb4eb806481e
Amit Kumar Mondal <admin@amitinside.com>
2015-06-17 04:22:32 +0200

    Refactored RPi Web UI for Permissions Added Functionality to add and remove permissions

--- RPi-Gateway/org.eclipse.kura.web/src/main/java/org/eclipse/kura/web/client/settings/PermissionsTab.java
+++ RPi-Gateway/org.eclipse.kura.web/src/main/java/org/eclipse/kura/web/client/settings/PermissionsTab.java
@@ -1,10 +1,12 @@
 package org.eclipse.kura.web.client.settings;
 
-import org.eclipse.kura.web.client.messages.Messages;
-import org.eclipse.kura.web.shared.GwtKuraErrorCode;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
 import org.eclipse.kura.web.shared.model.GwtSession;
 import org.eclipse.kura.web.shared.service.GwtCertificatesService;
-import org.eclipse.kura.web.shared.service.GwtCertificatesServiceAsync;
+import org.eclipse.kura.web.shared.service.GwtPermissionsService;
 
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
 import com.extjs.gxt.ui.client.Style.Scroll;
@@ -27,94 +29,97 @@ import com.extjs.gxt.ui.client.widget.layout.ColumnLayout;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import org.eclipse.kura.web.shared.service.GwtPermissionsServiceAsync;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.eclipse.kura.web.client.messages.Messages;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
 
 /**
  * This is used to display Permission Tab to revoke Permissions from Clients
+ * 
  * @author AMIT KUMAR MONDAL
  *
  */
 public class PermissionsTab extends LayoutContainer {
 
 	private static final Messages MSGS = GWT.create(Messages.class);
+	private final GwtPermissionsServiceAsync m_gwtPermissionsService = GWT.create(GwtPermissionsService.class);
 
-	private final static String SERVLET_URL = "/" + GWT.getModuleName() + "/file/certificate";
-	private final GwtCertificatesServiceAsync gwtCertificatesService = GWT.create(GwtCertificatesService.class);
+	private GwtSession m_currentSession;
+	private LayoutContainer m_commandInput;
+	private FormPanel m_formPanel;
+	private TextArea m_previouslyConnectedClients;
+	private TextArea m_permissionRevokedClients;
 
-	@SuppressWarnings("unused")
-	private GwtSession			m_currentSession;
-	private LayoutContainer 	m_commandInput;
-	private FormPanel			m_formPanel;
-	private TextArea			m_previouslyConnectedClients;
-	private TextArea			m_permissionRevokedClients;
+	private Button m_saveButton;
+	private Button m_resetButton;
+	private ButtonBar m_buttonBar;
+	
+	private String m_clientsList = "-- NO PERMISSION --";
 
-	private Button				m_executeButton;
-	private Button				m_resetButton;
-	private ButtonBar			m_buttonBar;
-
-
-	public PermissionsTab(GwtSession currentSession) 
-	{
+	public PermissionsTab(GwtSession currentSession) {
 		m_currentSession = currentSession;
 	}
 
-        
 	protected void onRender(Element parent, int index) {
-		super.onRender(parent, index);         
+		super.onRender(parent, index);
 		setLayout(new FitLayout());
-		setId("device-command");
+		setId("client-permissions");
 
 		FormData formData = new FormData("95% 40%");
 
-		//initToolBar();
-
-		//
-		// Command Form
-		//
 		m_formPanel = new FormPanel();
 		m_formPanel.setFrame(true);
 		m_formPanel.setHeaderVisible(false);
 		m_formPanel.setBorders(false);
 		m_formPanel.setBodyBorder(false);
-		m_formPanel.setAction(SERVLET_URL);
-		m_formPanel.setEncoding(Encoding.MULTIPART);
-		m_formPanel.setMethod(Method.POST);
-		//m_formPanel.setHeight("100.0%");
 
 		m_formPanel.setButtonAlign(HorizontalAlignment.RIGHT);
 		m_buttonBar = m_formPanel.getButtonBar();
 		initButtonBar();
 
-		//
-		// Initial description
-		// 
 		LayoutContainer description = new LayoutContainer();
 		description.setBorders(false);
 		description.setLayout(new ColumnLayout());
 
 		Label descriptionLabel = new Label(MSGS.RevokePermissionDescription());
-		
+
 		description.add(descriptionLabel);
 		description.setStyleAttribute("padding-bottom", "10px");
 		m_formPanel.add(description);
 
-		//
-		// Private Certificate
-		//       
 		m_previouslyConnectedClients = new TextArea();
 		m_previouslyConnectedClients.setBorders(true);
 		m_previouslyConnectedClients.setReadOnly(true);
 		m_previouslyConnectedClients.setEmptyText("");
 		m_previouslyConnectedClients.setName("");
+		
+		m_gwtPermissionsService.retrievePermissionFileData(new AsyncCallback<String>() {
+
+			public void onFailure(Throwable caught) {
+				Info.display(MSGS.error(), caught.getLocalizedMessage());
+			}
+
+			public void onSuccess(String result) {
+				m_clientsList = result;
+				Info.display(MSGS.info(), "Data Retrieved");
+			}
+			
+		});
+		
+		m_previouslyConnectedClients.setValue(m_clientsList);
 		m_previouslyConnectedClients.setAllowBlank(true);
 		m_previouslyConnectedClients.setFieldLabel(MSGS.clientList());
 		m_formPanel.add(m_previouslyConnectedClients, formData);
-		
-		//
-		//
-		//
+
 		m_permissionRevokedClients = new TextArea();
 		m_permissionRevokedClients.setName("");
 		m_permissionRevokedClients.setPassword(true);
@@ -122,8 +127,6 @@ public class PermissionsTab extends LayoutContainer {
 		m_permissionRevokedClients.setEmptyText("");
 		m_permissionRevokedClients.setFieldLabel(MSGS.permissionRevokedClientList());
 		m_formPanel.add(m_permissionRevokedClients, new FormData("95%"));
-
-
 
 		m_commandInput = m_formPanel;
 
@@ -134,22 +137,19 @@ public class PermissionsTab extends LayoutContainer {
 		deviceCommandPanel.setHeaderVisible(false);
 		deviceCommandPanel.setScrollMode(Scroll.AUTO);
 		deviceCommandPanel.setLayout(new FitLayout());
-		//deviceCommandPanel.setHeight("100%");
 
-		//deviceCommandPanel.setTopComponent(m_commandInput);
 		deviceCommandPanel.add(m_commandInput);
-		//deviceCommandPanel.add(m_publicCertificateArea);
 
 		add(deviceCommandPanel);
 	}
 
 	private void initButtonBar() {
-		m_executeButton = new Button(MSGS.deviceCommandExecute());
-		m_executeButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
-			@Override  
+		m_saveButton = new Button(MSGS.saveRevokedClients());
+		m_saveButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
+			@Override
 			public void componentSelected(ButtonEvent ce) {
 				if (m_formPanel.isValid()) {
-					//m_result.clear();
+					// m_result.clear();
 					m_commandInput.mask(MSGS.waiting());
 					m_formPanel.submit();
 				}
@@ -157,7 +157,7 @@ public class PermissionsTab extends LayoutContainer {
 		});
 
 		m_resetButton = new Button(MSGS.reset());
-		m_resetButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
+		m_resetButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
 			@Override
 			public void componentSelected(ButtonEvent ce) {
 				m_formPanel.reset();
@@ -165,7 +165,7 @@ public class PermissionsTab extends LayoutContainer {
 		});
 
 		m_buttonBar.add(m_resetButton);
-		m_buttonBar.add(m_executeButton);
+		m_buttonBar.add(m_saveButton);
 	}
 
 	public void refresh() {
