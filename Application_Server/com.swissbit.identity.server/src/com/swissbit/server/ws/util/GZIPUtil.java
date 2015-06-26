e5aab29706a0a74f5764a932469cdff58362866f
Amit Kumar Mondal <admin@amitinside.com>
2015-06-26 22:15:09 +0200

    Added GZIPUtil Class

---
+++ Application_Server/com.swissbit.identity.server/src/com/swissbit/server/ws/util/GZIPUtil.java
@@ -0,0 +1,13 @@
+package com.swissbit.server.ws.util;
+
+import java.io.File;
+
+public final class GZIPUtil {
+
+	public static File compressStringToGZip(final String[] str) {
+		// TODO Write code to compress string to file
+		return null;
+
+	}
+
+}
